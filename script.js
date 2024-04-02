3d5a921d41cfb87aa342c89b95a6929f4f267b3b
mothibimalebana <mothibi.mm12@gmail.com>
2024-04-02 14:42:34 +0200

    created a playRound function
    created a function called playround and created playerRound which prints win, lose or draw for each round

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 let gameChoices = ["Rock", "Paper", "Scissors"];
 
+//function to get a choice for computer
 function getComputerChoice () {
     return gameChoices[(Math.floor(Math.random() * gameChoices.length))];
+}
+
+//function to play a round
+function playRound (playerSelection, computerSelection) {
+    //playerSelection == "Rock"
+    if(playerSelection == "Rock" && computerSelection == "Rock")
+        console.log(`Draw, ${computerSelection} is the same as ${playerSelection}`);
+
+    else if(playerSelection == "Rock" && computerSelection == "Paper")
+        console.log(`You lose, ${computerSelection} beats ${playerSelection}`);
+
+    else if(playerSelection == "Rock" && computerSelection == "Scissor")
+        console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+
+    //playerSelection == "Rock"
+    else if(playerSelection == "Paper" && computerSelection == "Rock")
+        console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+
+    else if(playerSelection == "Paper" && computerSelection == "Paper")
+        console.log(`Draw, ${computerSelection} is the same as ${playerSelection}`);
+
+    else if(playerSelection == "Paper" && computerSelection == "Scissor")
+        console.log(`You lose, ${computerSelection} beats ${playerSelection}`);
+
+    //playerSelection == "Scissors"
+    
+
 }
