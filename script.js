2b664c5f862a611a4513e807c4b1038947be3535
mothibimalebana <mothibi.mm12@gmail.com>
2024-04-04 23:40:48 +0200

    completed gamePlay function
    Players can now play a game(Best out of 5), also they game now keeps track of scores

--- script.js
+++ script.js
@@ -1,47 +1,81 @@
 let gameChoices = ["Rock", "Paper", "Scissors"];
+let playerPoints = 0;
+let computerPoints = 0;
 
+//function to get player's choice
+function getPlayerChoice() {
+    playerSelection = prompt("Please enter a selection:", "Rock/ Paper/ Scissors")
+    playerSelection = playerSelection.toLowerCase();
+
+    return playerSelection;
+}
 //function to get a choice for computer
 function getComputerChoice () {
     return gameChoices[(Math.floor(Math.random() * gameChoices.length))];
 }
-
 //function to play a round
 function playRound (playerSelection, computerSelection) {
     //playerSelection == "Rock"
     if(playerSelection == "rock" && computerSelection == "Rock")
         console.log(`Draw, ${computerSelection} is the same as ${playerSelection}`);
 
-    else if(playerSelection == "rock" && computerSelection == "Paper")
+    else if(playerSelection == "rock" && computerSelection == "Paper"){
+        computerPoints++;
         console.log(`You lose, ${computerSelection} beats ${playerSelection}`);
+    }
 
-    else if(playerSelection == "rock" && computerSelection == "Scissors")
+    else if(playerSelection == "rock" && computerSelection == "Scissors"){
+        playerPoints++;
         console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+    }
 
     //playerSelection == "Rock"
-    else if(playerSelection == "paper" && computerSelection == "Rock")
+    else if(playerSelection == "paper" && computerSelection == "Rock"){
+        playerPoints++;
         console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+    }
 
     else if(playerSelection == "paper" && computerSelection == "Paper")
         console.log(`Draw, ${computerSelection} is the same as ${playerSelection}`);
 
-    else if(playerSelection == "paper" && computerSelection == "Scissors")
+    else if(playerSelection == "paper" && computerSelection == "Scissors"){
+        computerPoints++;
         console.log(`You lose, ${computerSelection} beats ${playerSelection}`);
+    }
 
     //playerSelection == "Scissors"
-    else if(playerSelection == "scissors" && computerSelection == "Rock")
+    else if(playerSelection == "scissors" && computerSelection == "Rock"){
+        computerPoints++;
         console.log(`You lose, ${computerSelection} beats ${playerSelection}`);
+    }
 
-    else if(playerSelection == "scissors" && computerSelection == "Paper")
+    else if(playerSelection == "scissors" && computerSelection == "Paper"){
+        playerPoints++;
         console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+    }
 
-    else if(playerSelection == "scissors" && computerSelection == "Scissors");
+    else if(playerSelection == "scissors" && computerSelection == "Scissors")
+        console.log(`Draw, ${computerSelection} is the same as ${playerSelection}`);    
 
     else
         console.log("Either you didn't spell Scissors right or you choose a invalid token")
 }
+//function to play a game (Best out of 5)
+function playGame() {
+    while((playerPoints < 5) && (computerPoints < 5)) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player points: ${playerPoints} \nComputer Points: ${computerPoints}`);
+    }
+    getWinner();
+}
+function getWinner(playerPoints, computerPoints) {
+    if (playerPoints < computerPoints)
+        console.log("Computer wins");
+    else
+        console.log("You win");
+}
 
-playerSelection = prompt("Please enter a selection:", "Rock/ Paper/ Scissors")
-playerSelection = playerSelection.toLowerCase();
+playGame();
 
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
