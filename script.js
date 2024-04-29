9f6bf862a064d6cd251b0d43f5a67e264424325a
mothibimalebana <mothibi.mm12@gmail.com>
2024-04-29 10:14:29 +0200

    fetched changes on a different pc

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let gameChoices = ["Rock", "Paper", "Scissors"];
+let playerPoints = 0;
+let computerPoints = 0;
 
 //function to get a choice for computer
 function getComputerChoice () {
     return gameChoices[(Math.floor(Math.random() * gameChoices.length))];
+}
+function winPlayer() {
+    return playerPoints++;
+}
+function winComputer() {
+    return computerPoints++;
 }
 
 //function to play a round
@@ -12,20 +20,32 @@ function playRound (playerSelection, computerSelection) {
         console.log(`Draw, ${computerSelection} is the same as ${playerSelection}`);
 
     else if(playerSelection == "rock" && computerSelection == "Paper")
-        console.log(`You lose, ${computerSelection} beats ${playerSelection}`);
+        {
+            console.log(`You lose, ${computerSelection} beats ${playerSelection}`);
+            winComputer();
+        }
 
     else if(playerSelection == "rock" && computerSelection == "Scissors")
-        console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+        {
+            console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+            winPlayer();
+        }
 
     //playerSelection == "Rock"
     else if(playerSelection == "paper" && computerSelection == "Rock")
-        console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+        {
+            console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+            winPlayer();
+        }
 
     else if(playerSelection == "paper" && computerSelection == "Paper")
         console.log(`Draw, ${computerSelection} is the same as ${playerSelection}`);
 
     else if(playerSelection == "paper" && computerSelection == "Scissors")
-        console.log(`You lose, ${computerSelection} beats ${playerSelection}`);
+        {
+            console.log(`You lose, ${computerSelection} beats ${playerSelection}`);
+            winComputer();
+        }
 
     //playerSelection == "Scissors"
     else if(playerSelection == "scissors" && computerSelection == "Rock")
@@ -44,4 +64,6 @@ playerSelection = prompt("Please enter a selection:", "Rock/ Paper/ Scissors")
 playerSelection = playerSelection.toLowerCase();
 
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+while(playerPoints != 5 || computerPoints != 5){
+playRound(playerSelection, computerSelection);
+}
